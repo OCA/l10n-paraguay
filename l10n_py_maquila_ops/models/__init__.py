91a50062cccdc36ff4969e918b3851d54d428219
mileo <mileo@kmee.com.br>
2026-04-08 13:13:12 -0300

    [ADD] l10n_py_maquila_ops: Temporary admission, export and fiscal operations for Maquila

---
+++ l10n_py_maquila_ops/models/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2026 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import maquila_admission
+from . import maquila_export
+from . import maquila_guarantee
+from . import maquila_program
+from . import account_move
+from . import account_payment
+from . import purchase_order
+from . import sale_order
+from . import stock_picking
